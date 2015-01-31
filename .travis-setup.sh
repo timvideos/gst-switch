3c73eabb0d3a3b7ca865c07c86b4fda2c9afe314
Tim 'mithro' Ansell <mithro@mithis.com>
2015-01-31 21:07:14 +1100

    SciPy requires PIL to read an image.

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -11,13 +11,13 @@ sudo apt-get -y install gstreamer1.0.* libgstreamer.*1.0.*
 # Python stuff for gst-switch API - Python 2.7
 sudo apt-get -y install python-software-properties python-pip
 sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 python-gi
-sudo apt-get -y install python-scipy
+sudo apt-get -y install python-scipy python-pil
 sudo pip install --upgrade -r requirements.txt
 
 # Python stuff for gst-switch API - Python 3.4
 sudo apt-get -y install python3-software-properties python3-pip
 sudo apt-get -y install libglib2.0-dev gir1.2-glib-2.0 libgirepository1.0-dev libglib2.0-0 python3-gi
-sudo apt-get -y install python3-scipy
+sudo apt-get -y install python3-scipy python3-pil
 sudo pip3 install --upgrade -r requirements.txt
 
 # Needed for tests
